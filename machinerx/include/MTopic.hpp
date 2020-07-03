69d0b814d0fe7d70e641311ddafaa2548f7df60d
alsaibie <alsaibie@gmail.com>
2020-07-03 20:47:40 +0300

    WIP MTopic configure

--- machinerx/include/MTopic.hpp
+++ machinerx/include/MTopic.hpp
@@ -56,7 +56,7 @@ namespace MachineRX {
 typedef uint32_t td_t;
 
 typedef struct _msgCore {
-    uint32_t tick_stamp{0};
+    uint32_t tick_stamp_ms{0};
     uint32_t msg_count{0};
 } MsgCore_T;
 
@@ -68,19 +68,22 @@ typedef struct {
 
 class MTopicBase_ {
    public:
-    inline const char *get_name() {
+    inline const char *getName() {
         return th.name;
     }
 
-    inline const td_t get_id() {
+    inline const td_t getId() {
         return th.id;
     }
 
-    inline const uint32_t get_size() {
+    inline const uint32_t getSize() {
         return th.size;
     }
 
-    static std::vector<MTopicHandle_t *> ptrMTH;
+    /* Requires C++17 */
+    inline static std::vector<MTopicHandle_t *> ptrMTH;
+    inline static timespec ts_start{0, 0};
+    inline static bool time_spec_offset{false};
 
    protected:
     MTopicBase_(MTopicHandle_t &th_, uint32_t msg_size) : th(th_) {
@@ -89,8 +92,14 @@ class MTopicBase_ {
             msgPtr = create_shared_memspace(msg_size);
             ptrMTH.push_back(&th);
         }
+        if (time_spec_offset == false) {
+            if (clock_gettime(CLOCK_MONOTONIC, &ts_start) != 0) {
+                // fail TODO
+            }
+        }
     }
     ~MTopicBase_() {
+        //TODO: pop out of vector ptrMTH
         destroy_shared_memspace();
     }
 
@@ -103,6 +112,17 @@ class MTopicBase_ {
 
     inline void destroy_shared_memspace() {
         free(msgPtr);  //TODO: replace with custom per platform dealloc
+    }
+
+    inline uint32_t timespec_to_ms(struct timespec &ts_f, struct timespec &ts_i) {
+        uint32_t t_ms = (ts_f.tv_sec - ts_i.tv_sec) * 1000L;
+        int32_t delta_tms = (ts_f.tv_nsec / 1000000L - ts_i.tv_nsec / 1000000L);
+        if (delta_tms >= 0) {
+            t_ms += delta_tms;
+        } else {
+            t_ms += 1000 + delta_tms;
+        }
+        return t_ms;
     }
 
     MTopicHandle_t th;
@@ -119,10 +139,15 @@ class MTopicPublisher : public MTopicBase_ {
     inline uint32_t publish(MTopicMsgT &msg) {
         MsgCore_T *msgCore = static_cast<MsgCore_T *>(&msg);
         msgCore->msg_count++;
-        msgCore->tick_stamp = 0;                // TODO: a posix way of getting a timestamp
+        struct timespec ts;
+        if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
+            // fail TODO
+        }
+        msgCore->tick_stamp_ms = timespec_to_ms(&ts, &ts_start);
         pthread_mutex_lock(&msg_access_mutex);  // TODO: replace with a timed lock, or try lock?
         memset(msgPtr, &msg, th.size);
         pthread_mutex_unlock(&msg_access_mutex);
+        // TODO: Implement a msg queue of a given length - subscribers may need to process a buffer
     }
 };
 
@@ -138,13 +163,12 @@ class MTopicSubscriber : public MTopicBase_ {
     }
 
     inline uint32_t read() {
-
         pthread_mutex_lock(&msg_access_mutex);  //TODO: replace with a timed lock, or try lock?
         memcpy(&msg, &msgPtr, th.size);
         pthread_mutex_unlock(&msg_access_mutex);
 
         MsgCore_T *msgCore = static_cast<MsgCore_T *>(&msg);
-        previous_tick_stamp = msgCore->tick_stamp;
+        previous_tick_stamp = msgCore->tick_stamp_ms;
         if (msgCore->msg_count > previous_msg_count) {
             previous_msg_count = msgCore->msg_count;
             cb(msg);
---
+++ machinerx/src/MachineRX.cpp
@@ -0,0 +1,5 @@
+#include "MachineRX.hpp"
+
+// std::vector<MachineRX::MTopicHandle_t *> MachineRX::MTopicBase_::ptrMTH;
+// struct timespec MachineRX::MTopicBase_::ts_start = {0, 0};
+// bool MachineRX::MTopicBase_::time_spec_offset = false;
